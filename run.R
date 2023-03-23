406c27e23c3862f12aa666d93cab2b4d74c1667a
Michael Škvrňák <michael.skvrnak@protonmail.com>
2023-03-23 21:13:27 +0100

    Update run.R

--- run.R
+++ run.R
@@ -3,6 +3,6 @@
 # See https://books.ropensci.org/targets/hpc.html
 # to learn about your options.
 
-# targets::tar_make()
+targets::tar_make()
 # targets::tar_make_clustermq(workers = 2) # nolint
-targets::tar_make_future(workers = 4) # nolint
+# targets::tar_make_future(workers = 4) # nolint
